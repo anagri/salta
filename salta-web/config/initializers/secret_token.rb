3e22aacad2f43621049e50fbef462a11528737ec
Amiruddin Nagri <anagri@thoughtworks.com>
2010-12-10 17:55:29 +0530

    [Amir] scaffolding for salta-web

---
+++ salta-web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaltaWeb::Application.config.secret_token = '152b849febc05fa5e7a159038f4834f8faef2169662a614f418ec2df71cea8da4b9306b48a298f003b83641b06a85cc897f4d89f1d9d0a01f122fdc4512b37ed'
